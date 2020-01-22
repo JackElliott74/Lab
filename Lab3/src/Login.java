439cc413167d4403080c7163d5b95c064597a3f8
Jacke <Jacke@146.85.226.81>
2020-01-22 11:10:52 -0500

    Intial Commit

---
+++ Lab3/src/Login.java
@@ -0,0 +1,36 @@
+
+/**
+ *
+ * @author Jacke
+ */
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet(urlPatterns={"/login"})
+public class Login extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        resp.setContentType("text/plain");
+        var pw = resp.getWriter();
+        var name = req.getParameter("user");
+        if( name == null ){
+            pw.printf("No username provided");
+        } else {
+            var sess = req.getSession();
+            sess.setAttribute("name", name );
+            pw.printf("Logged in as "+name);
+        }
+        var password = req.getParameter("password");
+        if( password == null ){
+            pw.printf("No password provided");
+        } else {
+            var sess = req.getSession();
+            sess.setAttribute("password", password );
+            pw.printf("Account created for "+name);
+        }
+    }
+
+}
